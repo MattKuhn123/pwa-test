7295893dbe7703c19d2284ad7d1440196f360289
Kuhn, Matthew L <mlkuhn@tva.gov>
2023-05-23 08:08:35 -0400

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-amm'`, () => {
+  it(`should have as title 'pwa-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-amm');
+    expect(app.title).toEqual('pwa-test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-amm app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pwa-test app is running!');
   });
 });
