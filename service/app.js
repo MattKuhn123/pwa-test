af5dfb6fa2cfdd912732f05fd184989966db0ec6
Kuhn, Matthew L <mlkuhn@tva.gov>
2023-05-30 11:32:23 -0400

    datagroups

--- service/app.js
+++ service/app.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 
-const habitatData = require('./data/habitats.json');
-const species = require('./data/species.json');
-const stations = require('./data/stations.json');
+const initHabitats = require('./data/habitats.json');
+const habitats = initHabitats;
+const initSpecies = require('./data/species.json');
+const species = initSpecies;
+const initStations = require('./data/stations.json');
+const stations = initStations;
 
 const app = express();
+
 app.use(cors());
+app.use(express.json())
+
 const port = 3000;
 
-app.get('/habitats', (req, res) => {
-  res.send(habitatData);
-})
+app.get('/habitats', (req, res) => res.send(habitats));
+app.post('/habitats', (req, res) => habitats.push(req.body));
 
-app.get('/species', (req, res) => {
-  res.send(species);
-})
+app.get('/species', (req, res) => res.send(species));
+app.post('/species', (req, res) => species.push(req.body));
 
-app.get('/stations', (req, res) => {
-  res.send(stations);
-})
+app.get('/stations', (req, res) => res.send(stations));
+app.post('/stations', (req, res) => stations.push(req.body));
 
-app.post('/session', (req, res) => {
-  console.log(req.body);
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
