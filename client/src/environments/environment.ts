fc0e7d9a845acf89e6f13794fd7ad60058fb8aa3
Kuhn, Matthew L <mlkuhn@tva.gov>
2023-06-06 10:26:20 -0400

    update env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: "http://mlk-pwa-test-svc-env.eba-tifr3hk3.us-east-1.elasticbeanstalk.com/",
+  api: "http://mlk-pwa-test-svc-env.eba-tifr3hk3.us-east-1.elasticbeanstalk.com",
   stations: '/stations',
   habitats: '/habitats',
   species: '/species',
